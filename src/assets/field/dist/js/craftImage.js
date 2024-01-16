b228bb2260dc55a0db85cedd8dbfe4a84390143e
Stemmer <stemmer@abm.at>
2024-01-16 13:43:30 +0100

    fixed regexp for image transforms

--- src/assets/field/dist/js/craftImage.js
+++ src/assets/field/dist/js/craftImage.js
@@ -221,15 +221,17 @@ const CraftImage = {
 
   getAssetUrlComponents: (url) => {
     const matches = url.match(
-      /(.*)#asset:(\d+):(url|transform):?([a-zA-Z][a-zA-Z0-9_]*)?/
+      /(.*)#asset:(\d+)(:(url|transform):([a-zA-Z][a-zA-Z0-9_]*)?)?/
     );
-    return matches
-      ? {
-          url: matches[1],
-          assetId: matches[2],
-          transform: matches[3] !== 'url' ? matches[4] : null,
-        }
-      : null;
+      
+    if (matches) {
+      return {
+        url: matches[1],
+        assetId: matches[2],
+        transform: matches[4] !== 'url' ? matches[5] : null,
+      };
+    }
+    return null;
   },
 
   getTransformUrl: (assetId, handle, callback) => {
